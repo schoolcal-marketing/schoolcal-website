3148b73a6e0464d6d86e3c88d786d6e27c5c97d1
Jeremiah Rizzo <rizzo@hey.com>
2025-08-11 16:20:10 -0400

    removed domain redirects

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'schoolcal.co',
-          },
-        ],
-        destination: 'https://schoolcal.com/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.schoolcal.co',
-          },
-        ],
-        destination: 'https://schoolcal.com/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.schoolcal.com',
-          },
-        ],
-        destination: 'https://schoolcal.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;

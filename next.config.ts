8f6486185bf71c09812e94e1fa550fd537b9ca5a
Jeremiah Rizzo <rizzo@hey.com>
2025-08-11 16:14:58 -0400

    301 redirect old urls in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'schoolcal.co',
+          },
+        ],
+        destination: 'https://schoolcal.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.schoolcal.co',
+          },
+        ],
+        destination: 'https://schoolcal.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.schoolcal.com',
+          },
+        ],
+        destination: 'https://schoolcal.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
